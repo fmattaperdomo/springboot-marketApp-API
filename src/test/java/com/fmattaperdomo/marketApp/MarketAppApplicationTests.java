40dd8082f427b23db1213deaf837b8adad5d2e63
fmattaperdomo <fmattaperdomo@gmail.com>
2024-12-15 23:22:26 -0500

    Project Structure added

---
+++ src/test/java/com/fmattaperdomo/marketApp/MarketAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmattaperdomo.marketApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
